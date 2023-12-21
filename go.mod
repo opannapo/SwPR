f0637f48e86bd5fafe6a66152dbf91bcc8eaa62a
opannapo <taufan.alfazri@gmail.com>
2023-12-21 17:12:10 +0700

    utils password with bcrypt include unittest

--- go.mod
+++ go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/lib/pq v1.10.9
 	github.com/oapi-codegen/runtime v1.1.0
 	github.com/spf13/viper v1.18.2
-	golang.org/x/sync v0.5.0
+	golang.org/x/crypto v0.16.0
 )
 
 require (
@@ -46,7 +46,6 @@ require (
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
-	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
---
+++ util/password_test.go
@@ -0,0 +1,75 @@
+package util
+
+import "testing"
+
+func TestCheckPasswordHash(t *testing.T) {
+	type args struct {
+		password string
+		hash     string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Success pwd 123456",
+			args: args{
+				password: "123456",
+				hash:     "$2a$12$ya8fM2J7lPJrDkcFI.jIi.jpOtg3hBC7otnK7k4pt8svU2/RvGCam",
+			},
+			want: true,
+		},
+		{
+			name: "Success pwd TestSawitPro!23$",
+			args: args{
+				password: "TestSawitPro!23$",
+				hash:     "$2a$12$FjtOZ.O9SaaKs6XArCM40uA2Y4mVVXfwpZOE8SOyg/ssx51/YEv16",
+			},
+			want: true,
+		},
+		{
+			name: "Error pwd TestSawitPro",
+			args: args{
+				password: "TestSawitPro",
+				hash:     "$2a$12$FjtOZ.O9SaaKs6XArCM40uA2Y4mVVXfwpZOE8SOyg/ssx51/YEv16",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CheckPasswordHash(tt.args.password, tt.args.hash); got != tt.want {
+				t.Errorf("CheckPasswordHash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHashPassword(t *testing.T) {
+	type args struct {
+		password string
+	}
+	tests := []struct {
+		name                 string
+		args                 args
+		isGeneratedHashValid bool
+	}{
+		{
+			name: "Success",
+			args: args{
+				password: "123456",
+			},
+			isGeneratedHashValid: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := HashPassword(tt.args.password)
+			actualMatch := CheckPasswordHash(tt.args.password, got)
+			if tt.isGeneratedHashValid != actualMatch {
+				t.Errorf("HashPassword() got = %v, want isMatch %v, actualMatch %v", got, tt.isGeneratedHashValid, actualMatch)
+			}
+		})
+	}
+}
