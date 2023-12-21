3c2d6537bd65acf4cb71e8622ee2f59ee2c42d7f
opannapo <taufan.alfazri@gmail.com>
2023-12-21 17:22:49 +0700

    update table structure password len 100

--- database.sql
+++ database.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users
 (
-    id         serial4     NOT NULL,
-    full_name  varchar(50) NOT NULL,
+    id         serial4      NOT NULL,
+    full_name  varchar(50)  NOT NULL,
     "password" varchar(100) NOT NULL,
-    phone      varchar(20) NOT NULL,
+    phone      varchar(20)  NOT NULL,
     created_at timestamptz NULL DEFAULT now(),
     updated_at timestamptz NULL,
     CONSTRAINT user_pk PRIMARY KEY (id),
