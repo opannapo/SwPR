d0dbca42c1e9f183c5d6c552561c24c4d9952ee6
opannapo <taufan.alfazri@gmail.com>
2023-12-21 16:30:46 +0700

    Unittest until ValidatePhoneForRegister

--- util/validation.go
+++ util/validation.go
@@ -4,7 +4,7 @@ import (
 	"regexp"
 )
 
-func ValidatePhoneForRegister(in string) (isMatch bool, err error) {
+func ValidatePhoneForRegister(in string) (isMatch bool) {
 	//pattern := `^\+62(?:\d{8,15}|-\d{8,15})$` // ini untuk limit 8-16 karakter
 	pattern := `^\+62(?:\d{10,13}|-\d{10,13})$`
 	re := regexp.MustCompile(pattern)
---
+++ util/validation_test.go
@@ -0,0 +1,60 @@
+package util_test
+
+import (
+	"swpr/util"
+	"testing"
+)
+
+func TestValidatePhoneForRegister(t *testing.T) {
+	type args struct {
+		phoneNumber string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantResult bool
+	}{
+		{
+			name: "Success 10 number",
+			args: args{
+				phoneNumber: "+621234567890",
+			},
+			wantResult: true,
+		},
+		{
+			name: "Success 12 number",
+			args: args{
+				phoneNumber: "+62123456789012",
+			},
+			wantResult: true,
+		},
+		{
+			name: "Success 13 number",
+			args: args{
+				phoneNumber: "+621234567890123",
+			},
+			wantResult: true,
+		},
+		{
+			name: "Error 9 number",
+			args: args{
+				phoneNumber: "+62123456789",
+			},
+			wantResult: false,
+		},
+		{
+			name: "Error 14 number",
+			args: args{
+				phoneNumber: "+6212345678901234",
+			},
+			wantResult: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if actualResult := util.ValidatePhoneForRegister(tt.args.phoneNumber); actualResult != tt.wantResult {
+				t.Errorf("TestValidatePhoneForRegister() = %v, want %v", actualResult, tt.wantResult)
+			}
+		})
+	}
+}
