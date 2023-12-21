08f4784225de47986b180dd7e300cab4e8e427b9
opannapo <taufan.alfazri@gmail.com>
2023-12-21 16:23:34 +0700

    Fix validation requirement 10-13 char

--- util/validation.go
+++ util/validation.go
@@ -5,13 +5,13 @@ import (
 )
 
 func ValidatePhoneForRegister(in string) (isMatch bool, err error) {
-	pattern := `^\+62(?:\d{8,15}|-\d{8,15})$`
+	//pattern := `^\+62(?:\d{8,15}|-\d{8,15})$` // ini untuk limit 8-16 karakter
+	pattern := `^\+62(?:\d{10,13}|-\d{10,13})$`
 	re := regexp.MustCompile(pattern)
 	isMatch = re.MatchString(in)
 	return
 }
 
 func ValidateNameForRegister(in string) (err error) {
-
 	return
 }
