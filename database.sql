c61fc2ace605897eb0bf2c7e1d2224838f84de9c
opannapo <taufan.alfazri@gmail.com>
2023-12-21 17:39:45 +0700

    new endpoint & table login_attempt

--- database.sql
+++ database.sql
@@ -9,3 +9,14 @@ CREATE TABLE users
     CONSTRAINT user_pk PRIMARY KEY (id),
     CONSTRAINT user_un UNIQUE (phone)
 );
+
+CREATE TABLE login_attempt
+(
+    id         serial  NOT NULL,
+    user_id    int8    NOT NULL,
+    status     boolean NOT NULL,
+    created_at timestamptz NULL DEFAULT now(),
+    CONSTRAINT login_attempt_pk PRIMARY KEY (id),
+    CONSTRAINT login_attempt_fk FOREIGN KEY (user_id) REFERENCES users (id)
+);
+CREATE INDEX login_attempt_user_id_idx ON login_attempt (user_id, status);
