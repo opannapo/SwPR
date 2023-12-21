67b758f7529b59ed4ffc9eb6e3d89c2842df4068
opannapo <taufan.alfazri@gmail.com>
2023-12-21 13:48:46 +0700

    update mod & sql script

--- database.sql
+++ database.sql
@@ -1,17 +1,11 @@
-/**
-  This is the SQL script that will be used to initialize the database schema.
-  We will evaluate you based on how well you design your database.
-  1. How you design the tables.
-  2. How you choose the data types and keys.
-  3. How you name the fields.
-  In this assignment we will use PostgreSQL as the database.
-  */
-
-/** This is test table. Remove this table and replace with your own tables. */
-CREATE TABLE test (
-	id serial PRIMARY KEY,
-	name VARCHAR ( 50 ) UNIQUE NOT NULL,
+CREATE TABLE users
+(
+    id         serial4     NOT NULL,
+    full_name  varchar(50) NOT NULL,
+    "password" varchar(50) NOT NULL,
+    phone      varchar(20) NOT NULL,
+    created_at timestamptz NULL DEFAULT now(),
+    updated_at timestamptz NULL,
+    CONSTRAINT user_pk PRIMARY KEY (id),
+    CONSTRAINT user_un UNIQUE (phone)
 );
-
-INSERT INTO test (name) VALUES ('test1');
-INSERT INTO test (name) VALUES ('test2');
